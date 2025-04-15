3e730f2ee014cd7aef873c822f4d0d1062ad5441
AbdulAhad50 <aabdulahad142@gmail.com>
2025-04-15 01:10:31 -0700

    assignment00-07

---
+++ assignment00_to_07/00_intro_python/02_agreement_bot.py
@@ -0,0 +1,8 @@
+def main():
+    animal = input("\033[1;3m What's your favourite animal____?\033[0m")
+    print(f"My favourite animal is also {animal}")
+
+
+
+if __name__ == 'main':
+    main()
---
+++ assignment00_to_07/00_intro_python/03_fahrenheit_to_celsius.py
@@ -0,0 +1,12 @@
+def main():
+    fehrenheit = float(input('Enter temperature in ferenheit: '))
+    celcius = (fehrenheit - 32) * 5.0/9.0
+    print(f"The Temperature {fehrenheit}F = {celcius} ")
+
+if __name__ == 'main':
+    main()
+
+
+
+
+    
